1515d9ecf63aa7452f6ef00c31b39be3085ffc36
mizik1 <mattk29@gmail.com>
2024-12-19 11:41:59 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-
 import Start from "./components/Start";
 import Chat from "./components/Chat";
 
@@ -9,40 +8,11 @@ const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.blue}>Hello World!</Text>
-      <Text style={styles.bigRed}>How are you?</Text>
-      <Text style={styles.bigRedBold}>I'm feeling blue!</Text>
-
-      <View style={styles.box}></View>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Start">
+        <Stack.Screen name="Start" component={Start} />
+        <Stack.Screen name="Chat" component={Chat} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  blue: {
-    color: "blue",
-    fontWeight: "600",
-  },
-  bigRed: {
-    color: "red",
-    fontSize: 30,
-  },
-  bigRedBold: {
-    color: "red",
-    fontSize: 30,
-    fontWeight: "600",
-  },
-  box: {
-    width: 60,
-    height: 60,
-    backgroundColor: "blue",
-    marginTop: 20, // Adds spacing between the text and the box
-  },
-});
